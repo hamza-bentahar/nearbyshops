669b7937d20b5984636066cb97b43cd15f69a4b4
Hamza Bentahar <59384@aui.ma>
2021-09-06 23:45:20 +0100

    feat: setting up models

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class Shop(models.Model):
+    picture = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=8, decimal_places=2)
+    longitude = models.DecimalField(max_digits=8, decimal_places=2)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='ShopUser')
+
+
+class ShopUser(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    like = models.BooleanField()
--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,15 @@ WSGI_APPLICATION = 'nearbyshops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nearbyshops',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'TEST': {
+            'NAME': 'testnearbyshops',
+        },
     }
 }
 
