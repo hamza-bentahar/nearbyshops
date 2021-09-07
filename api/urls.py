fc2b4fe781f51e150b238e12a1dceb2d7956c42f
Hamza Bentahar <59384@aui.ma>
2021-09-07 08:29:52 +0100

    feat: retrieving shops information

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_overview, name='api-overview')
+    path('', views.api_overview, name='api-overview'),
+    path('shops/', views.shop_list, name='shop-list'),
+    path('shops/<str:shop_id>/', views.shop_detail, name='shop-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ShopSerializer
+from .models import Shop
 
 
 @api_view(['GET'])
@@ -12,3 +14,17 @@ def api_overview(request):
         'dislike': '/dislike'
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def shop_list(request):
+    shops = Shop.objects.all()
+    serializer = ShopSerializer(shops, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def shop_detail(request, shop_id):
+    shops = Shop.objects.get(id=shop_id)
+    serializer = ShopSerializer(shops, many=False)
+    return Response(serializer.data)
