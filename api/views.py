7779bed818268754a78ffd11a0ad66929dbc420b
Hamza Bentahar <59384@aui.ma>
2021-09-10 19:22:26 +0100

    feat: add pagination

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .serializers import ShopSerializer, ShopUserSerializer
 from .models import Shop
 
