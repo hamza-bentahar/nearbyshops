22d3adc56695d2a0d833fefcd2cf7ce8208ad442
Hamza Bentahar <59384@aui.ma>
2021-09-10 19:17:15 +0100

    fix: decimals for longitude and latitude, combined unique values for shop and user

--- api/models.py
+++ api/models.py
@@ -8,8 +8,8 @@ class Shop(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    latitude = models.DecimalField(max_digits=8, decimal_places=2)
-    longitude = models.DecimalField(max_digits=8, decimal_places=2)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    latitude = models.DecimalField(max_digits=7, decimal_places=5)
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='ShopUser')
 
 
@@ -17,3 +17,6 @@ class ShopUser(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     like = models.BooleanField()
+
+    class Meta:
+        unique_together = ('shop', 'user',)
