2f5a0dffdafe5ebb709b699aa2984fa02a8fa0aa
Hamza Bentahar <59384@aui.ma>
2021-09-11 19:24:27 +0100

    refactor: create shop component

---
+++ frontend/src/components/Shop.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          {{ value.city }}
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          {{ value.name }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ value.distance === -1? '' : value.distance + ' kms away'}}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey">
+        <img
+          v-if="value.picture"
+          :alt="value.name"
+          :src="value.picture"
+        >
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text>Like</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Shop',
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
