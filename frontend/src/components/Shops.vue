2b222ee74778ee4f149db0d4b7fb05068362a571
Hamza Bentahar <59384@aui.ma>
2021-09-11 19:37:55 +0100

    feat: set up pagination

--- frontend/src/components/Shops.vue
+++ frontend/src/components/Shops.vue
@@ -1,9 +1,21 @@
 <template>
-  <v-row>
-    <v-col cols="12" sm="6" md="4" v-for="shop in shops.results" :key="shop.id">
-      <shop :value="shop"></shop>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" v-for="shop in shops.results" :key="shop.id">
+        <shop :value="shop"></shop>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col>
+        <v-pagination
+          v-model="page"
+          :length="shops.total_pages"
+          :total-visible="7"
+          @input="next"
+        ></v-pagination>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -17,14 +29,28 @@ export default {
   },
   data: () => ({
     shops: {},
+    page: 1,
   }),
   async mounted() {
-    try {
-      const shops = await axios.get('/api/shops');
-      this.shops = shops.data;
-    } catch (e) {
-      console.log(e);
-    }
+    await this.fetchShops();
+  },
+  methods: {
+    async fetchShops() {
+      try {
+        const shops = await axios.get('/api/shops', {
+          params: {
+            page: this.page,
+          },
+        });
+        this.shops = shops.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async next(value) {
+      this.page = value;
+      await this.fetchShops();
+    },
   },
 };
 </script>
