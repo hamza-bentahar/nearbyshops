7d22eaf63d7ebd0ad1c98c5ab5d5752cb2ccc603
Hamza Bentahar <59384@aui.ma>
2021-09-25 22:44:19 +0100

    feat: setting up unit tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ShopTest(APITestCase):
+    fixtures = ['shops_list.json']
+
+    def test_get_shops(self):
+        response = self.client.get(reverse('shop-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
