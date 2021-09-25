ba2bfbbf1b0b998a1bb3197ff7aac63793aaa02f
Hamza Bentahar <59384@aui.ma>
2021-09-25 22:55:21 +0100

    feat: setting up unit tests for shop details

--- api/tests.py
+++ api/tests.py
@@ -6,6 +6,10 @@ from rest_framework.test import APITestCase
 class ShopTest(APITestCase):
     fixtures = ['shops_list.json']
 
-    def test_get_shops(self):
+    def test_get_shop_list(self):
         response = self.client.get(reverse('shop-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_shop_details(self):
+        response = self.client.get(reverse('shop-detail', kwargs={'pk': 1}), format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
